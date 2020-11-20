2c90c21ca15dc3719b218e4e1fd602789a8ebdc4
Corentin Chatillon <corentin.chatillon@gmail.com>
2020-11-20 02:01:22 +0100

    init

---
+++ src/components/root.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+export function Root() {
+    return (
+        <div>this is the root component</div>
+    )
+} 
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path")
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.tsx",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "dashboard.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader"
+            },
+        ]
+    },
+    resolve: {
+        extensions: [".js", ".ts", ".tsx"]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: 'Production',
+        template: './template.html'
+    })]
+}   
